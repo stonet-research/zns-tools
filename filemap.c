6df4b34ab2eb7dccfe521724362bfae4a4b6e224
Nick Tehrany <nicktehrany1@gmail.com>
2022-09-10 14:57:35 +0200

    Fix extent handling
    TODO: fix wrong write pointers in zone info print

--- filemap.c
+++ filemap.c
@@ -10,7 +10,21 @@
 #include <sys/sysmacros.h>
 #include <linux/fs.h>
 #include <linux/blkzoned.h>
-#include "linux/fiemap.h"
+#include <linux/fiemap.h>
+
+/* 
+ * F2FS treats all devices as one address space, therefore if we use
+ * a conventional device and a ZNS, extent maps contain the offset within
+ * this total address space. For instance, with a 4GB conventional device
+ * and a ZNS the offsets in the ZNS address space are +4GB. Therefore, we
+ * need to find the size of the conventional space and subtract it from
+ * the extent mapping offsets.
+ *
+ * TODO: what if orders are different? this would only work with a single ZNS
+ * after a conventional device!
+ * 
+ * */
+uint64_t offset = 0;
 
 struct stat * get_stats(int fd, char *filename) {
     struct stat *file_stat;
@@ -26,6 +40,7 @@ struct stat * get_stats(int fd, char *filename) {
     return file_stat;
 }
 
+// TODO: is there a better way to get dev from major:minor than reading sysfs?
 char * get_dev_name(int major, int minor) {
     FILE *dev_info;
     char file_path[50];
@@ -36,7 +51,7 @@ char * get_dev_name(int major, int minor) {
 
     dev_info = fopen(file_path, "r");
     if (!dev_info) {
-        printf("\033[0;31mError\033[0m finding device for %u:%u in /sys/dev/block/", major, minor);
+        fprintf(stderr, "\033[0;31mError\033[0m finding device for %u:%u in /sys/dev/block/", major, minor);
         return NULL;
     }
     
@@ -66,9 +81,6 @@ char * get_dev_name(int major, int minor) {
  * */
 uint64_t get_zone_size(char *dev_name) {
     char *dev_path = NULL;
-    unsigned long long start_sector = 0;
-    struct blk_zone_report *hdr = NULL;
-    int nr_zones = 1;
     uint64_t zone_size = 0;
 
     dev_path = malloc(sizeof(char *) * (strlen(dev_name) + 6));
@@ -79,22 +91,13 @@ uint64_t get_zone_size(char *dev_name) {
         return 1;
     }
 
-    hdr = calloc(1, sizeof(struct blk_zone_report) + nr_zones + sizeof(struct blk_zone));
-    hdr->sector = start_sector;
-    hdr->nr_zones = nr_zones;
-
-    if (ioctl(dev_fd, BLKREPORTZONE, hdr) < 0) {
-        fprintf(stderr, "\033[0;31mError\033[0m getting Zone Size\n");
+    if (ioctl(dev_fd, BLKGETZONESZ, &zone_size) < 0) {
         return -1;
     }
 
-    zone_size = hdr->zones[0].capacity;
     close(dev_fd);
-
     free(dev_path);
-    free(hdr);
     dev_path = NULL;
-    hdr = NULL;
 
     return zone_size;
 }
@@ -143,7 +146,7 @@ int is_zoned(char *dev_name) {
 }
 
 /* 
- * Calculate the zone number of an LBA
+ * Calculate the zone number (starting with zone 1) of an LBA
  *
  * @lba: LBA to calculate zone number of
  * @zone_size: size of the zone
@@ -187,7 +190,7 @@ int print_zone_info(char *dev_name, uint32_t zone, uint64_t zone_size) {
         return -1;
     }
 
-    hdr = calloc(1, sizeof(struct blk_zone_report) + zone + sizeof(struct blk_zone));
+    hdr = calloc(1, sizeof(struct blk_zone_report) + sizeof(struct blk_zone));
     hdr->sector = start_sector;
     hdr->nr_zones = 1;
 
@@ -196,7 +199,6 @@ int print_zone_info(char *dev_name, uint32_t zone, uint64_t zone_size) {
         return -1;
     }
 
-    zone_size = hdr->zones[0].capacity;
     zone_mask = ~(zone_size - 1);
     printf("LBAS: 0x%06"PRIx64"  ZONE CAP: 0x%06"PRIx64"  WP: 0x%06"PRIx64"  ZONE MASK: 0x%06"PRIx32"\n\n",
             hdr->zones[0].start, hdr->zones[0].capacity, hdr->zones[0].wp, zone_mask);
@@ -212,6 +214,37 @@ int print_zone_info(char *dev_name, uint32_t zone, uint64_t zone_size) {
 }
 
 /* 
+ * Get the size of a device in bytes.
+ *
+ * @dev_name: device name (e.g., nvme0n2)
+ *
+ * returns: uint64_t size of the device in bytes.
+ *
+ * */
+uint64_t get_dev_size(char *dev_name) {
+    char *dev_path = NULL;
+    uint64_t dev_size = 0;
+
+    dev_path = malloc(sizeof(char *) * (strlen(dev_name) + 6));
+    snprintf(dev_path, strlen(dev_name) + 6, "/dev/%s", dev_name);
+
+    int dev_fd = open(dev_path, O_RDONLY);
+    if (dev_fd < 0) {
+        return 1;
+    }
+
+    if (ioctl(dev_fd, BLKGETSIZE64, &dev_size) < 0) {
+        return -1;
+    }
+
+    close(dev_fd);
+    free(dev_path);
+    dev_path = NULL;
+
+    return dev_size;
+}
+
+/* 
  * Get the file extents with FIEMAP ioctl
  *
  * @fd: file descriptor of the file
@@ -223,56 +256,57 @@ int print_zone_info(char *dev_name, uint32_t zone, uint64_t zone_size) {
  * */
 int get_extents(int fd, char *dev_name, struct stat *stats) {
     struct fiemap *fiemap;
-    uint64_t zone_size = 0;
+    uint8_t last_ext = 0;
     uint32_t zone = 0;
     uint32_t current_zone = 0;
-
-    fiemap = (struct fiemap*) calloc(sizeof(struct fiemap), sizeof (char *));
-
-    fiemap->fm_start = 0;
-    fiemap->fm_length = (stats->st_blocks >> 9);
-    fiemap->fm_flags = 0;
-	fiemap->fm_extent_count = 0;
-	fiemap->fm_mapped_extents = 0;
-
-    if (ioctl(fd, FS_IOC_FIEMAP, fiemap) < 0) {
-		return -1;
-	}
-
-    // make sure we have space for all extents
-	fiemap = (struct fiemap*) realloc(fiemap,sizeof(struct fiemap) + 
-            sizeof(struct fiemap_extent) * (fiemap->fm_mapped_extents));
-
-	fiemap->fm_extent_count = fiemap->fm_mapped_extents;
-	fiemap->fm_mapped_extents = 0;
-
-    if (ioctl(fd, FS_IOC_FIEMAP, fiemap) < 0) {
-		return -1;
-	}
-
-    printf("\nNumber of extents: %d\n", fiemap->fm_mapped_extents);
+    uint64_t ext_ctr = 1;
+    uint64_t zone_size = 0;
+    uint64_t phy_blk = 0;
+    uint64_t logical_blk = 0;
+    uint64_t len = 0;
 
     zone_size = get_zone_size(dev_name);
-    if (zone_size == 0) {
-		fprintf(stderr, "\033[0;31mError\033[0m getting Zone Size\n");
-        return -1;
-    }
 
-    for (int i = 0; i < fiemap->fm_mapped_extents; i++) {
-        zone = get_zone_number((fiemap->fm_extents[i].fe_physical >> 9), zone_size);
+    fiemap = (struct fiemap *) calloc(sizeof(struct fiemap) + sizeof(struct fiemap_extent), sizeof (char *));
+
+    fiemap->fm_flags = FIEMAP_FLAG_SYNC;
+    fiemap->fm_start = 0;
+    fiemap->fm_extent_count = 1; // get extents individually
+    fiemap->fm_length = (stats->st_blocks << 9);
+
+    do {
+        if (ioctl(fd, FS_IOC_FIEMAP, fiemap) < 0) {
+            return -1;
+        }
+
+        if (fiemap->fm_mapped_extents == 0) {
+            // TODO: have to use fibmap. When does this even happen?
+            printf("no extents");
+			return 0;
+        }
+
+        phy_blk = (fiemap->fm_extents[0].fe_physical - offset) >> 9;
+        logical_blk = fiemap->fm_extents[0].fe_logical >> 9;
+        len = fiemap->fm_extents[0].fe_length >> 9;
+
+        if (fiemap->fm_extents[0].fe_flags & FIEMAP_EXTENT_LAST) {
+            last_ext = 1;
+        }
+
+        zone = get_zone_number(phy_blk, zone_size);
 
         if (zone != current_zone) {
             printf("\n#### ZONE %u ####\n", zone);
-            print_zone_info(dev_name, zone + 1, zone_size);
+            print_zone_info(dev_name, zone, zone_size);
             current_zone = zone;
         }
-        printf("Extent %d:  Starting PBA: 0x%06"PRIx64"  Ending PBA: 0x%06"PRIx64"  Size: 0x%06"PRIx64"\n",
-                i, (fiemap->fm_extents[i].fe_physical >> 9), ((fiemap->fm_extents[i].fe_physical >> 9) + 
-                    (fiemap->fm_extents[i].fe_length >> 9)), fiemap->fm_extents[i].fe_length >> 9);
-    }
 
-    free(fiemap);
-    fiemap = NULL;
+        printf("EXTENT %d:  PBAS: 0x%06"PRIx64"  PBAE: 0x%06"PRIx64"  SIZE: 0x%06"PRIx64"\n",
+                ext_ctr, phy_blk, (phy_blk + len), len);
+
+        ext_ctr++;
+        fiemap->fm_start = ((logical_blk << 9) + (len << 9));
+    } while (last_ext == 0);
 
     return 0;
 }
@@ -293,14 +327,18 @@ int main(int argc, char *argv[]) {
     strcpy(filename, argv[1]);
 
     fd = open(filename, O_RDONLY);
-    fsync(fd); // make sure it is flushed to device
 
     if (fd < 0) {
-        printf("Failed opening file %s\n", filename);
+        fprintf(stderr, "\033[0;31mError\033[0m failed opening file %s\n", filename);
         return 1;
     }
     
     stats = get_stats(fd, filename);
+
+    if (!stats) {
+        fprintf(stderr, "\033[0;31mError\033[0m failed fstat on file %s\n", filename);
+        return 1;
+    }
 
     dev_name = get_dev_name(major(stats->st_dev), minor(stats->st_dev));
     dev_name[strcspn(dev_name, "\n")] = 0;
@@ -327,12 +365,14 @@ int main(int argc, char *argv[]) {
     }
 
     if (is_zoned(zns_dev_name) < 0) {
-        printf("\033[0;31mError\033[0m: %s is not a ZNS device\n", zns_dev_name);
+        fprintf(stderr, "\033[0;31mError\033[0m: %s is not a ZNS device\n", zns_dev_name);
         return 1;
     }
+
+    offset = get_dev_size(dev_name);
     
     if (get_extents(fd, zns_dev_name, stats) < 0) {
-        printf("\033[0;31mError\033[0m retrieving extents for %s\n", filename);
+        fprintf(stderr, "\033[0;31mError\033[0m retrieving extents for %s\n", filename);
         return 1;
     }
 
