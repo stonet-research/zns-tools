0456ef62d25ea3f9bd38a55e751e642039b8c765
Nick Tehrany <nicktehrany1@gmail.com>
2022-11-14 21:35:54 +0000

    clang format

--- lib/libzns-tools.c
+++ lib/libzns-tools.c
@@ -599,7 +599,10 @@ found:
 
     uint32_t zone = get_zone_number(cur_segment << ctrl.segment_shift);
     if (file_counter_map->file[i].last_zone != zone) {
-        file_counter_map->file[i].zone_ctr += (num_segments * F2FS_SEGMENT_BYTES >> ctrl.sector_shift) / zone_cap + 1;
+        file_counter_map->file[i].zone_ctr +=
+            (num_segments * F2FS_SEGMENT_BYTES >> ctrl.sector_shift) /
+                zone_cap +
+            1;
         file_counter_map->file[i].last_zone = zone;
     }
 }
